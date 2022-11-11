315b58bd1cf1935b5a57bfed0a358d46efa7e950
Viphakone Monty <10835390@my.uvu.edu>
2022-11-11 11:40:43 -0700

    started project and app with django

---
+++ wine_festival_schedule_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class WineFestivalScheduleAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'wine_festival_schedule_app'
