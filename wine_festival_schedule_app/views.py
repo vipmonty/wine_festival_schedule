34b6ae40ef3ba1d0cdb8af07d340ea672d6eda07
Viphakone Monty <10835390@my.uvu.edu>
2022-11-11 12:06:12 -0700

    after succesful index.html and style.css {% load static %}

--- wine_festival_schedule_app/views.py
+++ wine_festival_schedule_app/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def home(request):
+    return render(request, 'wine_festival_schedule_app/index.html')
--- wine_festival_schedule_root/settings.py
+++ wine_festival_schedule_root/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'wine_festival_schedule_app',
 ]
 
 MIDDLEWARE = [
